9452873e487f8fd0e821b88022840d9b99d838ae
Han Mai <h.mai@mpie.de>
2023-10-14 20:04:31 +0200

    executables

--- actual_usage/build_and_show_db.sh
+++ actual_usage/build_and_show_db.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <folder_path>"
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <folder_path> [--extract] [--max_dir_count <value>]"
     exit 1
 fi
 
 folder_path="$1"
+shift 1  # Remove the first argument (folder_path) from the list of arguments
 
 # Setonix
-path_to_utils="/scratch/pawsey0380/hmai/utils/actual_usage/"
+#path_to_utils="/scratch/pawsey0380/hmai/utils/actual_usage"
 # Garching
 path_to_utils="/cmmc/u/hmai/personal_dev/utils/actual_usage"
 
 # Run the first Python script and append the output to py.output
-python "$path_to_utils/build_vasp_database.py" "$folder_path"
+python "$path_to_utils/build_vasp_database.py" "$folder_path" "$@"
 
 # Run the second Python script and append the output to py.output
 python "$path_to_utils/summarise_vasp_database.py" "$folder_path"
