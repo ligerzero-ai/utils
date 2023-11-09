20c6669308506b41cd8fe53f90ed704ea339621d
Han Lin Mai <h.mai@mpie.de>
2023-11-09 22:30:06 +0100

    resubmitter refactoring

---
+++ jobfile.py
@@ -0,0 +1,110 @@
+import os
+import shutil
+
+def create_folder(directory, delete_folder=False):
+    """
+    Create a folder if it doesn't exist, and optionally delete it if it does.
+
+    Parameters:
+    - directory (str): The path of the folder to be created.
+    - delete_folder (bool): If True, delete the folder if it already exists.
+
+    Returns:
+    - None
+    """
+    if os.path.exists(directory):
+        if delete_folder:
+            print("Removing directory...")
+            if os.path.isdir(directory):
+                shutil.rmtree(directory)
+            elif os.path.isfile(directory):
+                os.remove(directory)
+            else:
+                print("Given path is a special file - manually remove")
+        else:
+            # Folder already exists, no replacement/deletion
+            print("No replacement/deletion created due to folder existing")
+    else:
+        os.makedirs(directory)
+                
+class jobfile:
+    '''
+    Class for jobfile object for passing into createJobFolder
+
+    Attributes:
+    file_path: string, is the path to jobfile template (switch for normal VASP calcs vs DDEC6)
+    HPC: string, one of "Gadi", "Setonix", or "Magnus"  This ensures that correct jobsubmission script is used.
+    VASP_version: string (one of: "5.4.4", "5.4.4-metagga", "6.1.0", "6.2.1", "6.3.0", "6.3.1")
+                  NOT ALL ARE AVAILABLE ON ALL SYSTEMS
+                  for loading module - defaults to "5.4.4"
+                  AVAILABILITY:
+                  for Gadi (5.4.4, 5.4.4-metagga, 6.1.0, 6.2.1, 6.3.3)
+                  for Magnus (5.4.4, 6.2.0)
+                  for Setonix (5.4.4, 6.2.1, 6.3.0)
+    CPU: integer
+         number of CPUs to use in job,
+         Gadi: 48 is 1 node  (Only use in full nodes, as you are charged for full nodes)
+         Magnus: 24 is 1 node (Only use in full nodes, as you are charged for full nodes)
+         Setonix: 128 is 1 node (Charged on a per-cpu hour basis, not per-node like Gadi)
+    RAM: RAM to be allocated - this is only specified in the case of Gadi, Setonix + magnus do not need specification.
+    walltime: INTEGER ONLY
+              The walltime of the job in hours
+    '''
+    def __init__(self,
+                 file_path,
+                 HPC = "Gadi",
+                 VASP_version = "5.4.4",
+                 CPU = 192,
+                 cpu_per_node = 48,
+                 RAM = 64,
+                 walltime = 999,
+                 max_resubmissions = 999):
+        self.file_path = file_path
+        self.HPC = HPC
+        self.VASP_version = VASP_version
+        self.CPU = CPU
+        self.RAM = RAM
+        self.walltime = walltime
+        self.max_resubmissions = max_resubmissions
+        self.cpu_per_node = cpu_per_node
+
+    def to_file(self,\
+                job_name = 'template_job',\
+                output_path = os.path.join(os.getcwd(), "test")):
+        """
+        """
+
+        create_folder(output_path)
+
+        with open("%s" % (self.file_path), 'r') as fin :
+            filedata = fin.read()
+        fin = open("%s" % (self.file_path), "rt", newline="\n")
+        # Replace the target string
+        filedata = filedata.replace("{WALLTIMESTRING}", "%s:00:00" % self.walltime)
+        filedata = filedata.replace("{CPUSTRING}", str(self.CPU))
+        filedata = filedata.replace("{MAXCONVITERATIONS}", str(self.max_resubmissions-1))
+
+        # Only on GADI
+        filedata = filedata.replace("{MEMORYSTRING}", "%sGB" % self.RAM)
+
+        if self.CPU <= self.cpu_per_node:
+            filedata = filedata.replace("{NODESTRING}", "1")
+        else:
+            filedata = filedata.replace("{NODESTRING}", "%s" % int(self.CPU/self.cpu_per_node))
+            
+        filedata = filedata.replace("{CASESTRING}", "%s" % job_name)
+
+        if self.VASP_version == "5.4.4":
+            filedata = filedata.replace("{VASPMODULELOADSTRING}", 'module load vasp/%s' %  self.VASP_version)
+        else:
+            if self.HPC == "Setonix" and self.VASP_version in ["6.3.0", "6.2.1"]:
+                filedata = filedata.replace("{VASPMODULELOADSTRING}", 'module load vasp6/%s' % self.VASP_version)
+            else:
+                filedata = filedata.replace("{VASPMODULELOADSTRING}", 'module load vasp/%s' % self.VASP_version)
+
+        # Write the file out again
+        with open(os.path.join(output_path, job_name), 'w') as fout:
+            fout.write(filedata)
+
+        fin.close()
+        fout.close()
--- vasp_resubmitter.py
+++ vasp_resubmitter.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 from utils.vasp import find_vasp_directories, check_convergence
 from utils.generic import get_latest_file_iteration
-
+from utils.jobfile import jobfile
 def get_slurm_jobs_working_directories(username="hmai"):
     command = f"squeue -u {username} -o \"%i %Z\""
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
@@ -33,7 +33,12 @@ class CalculationConverger():
     def submit_to_queue(self, dirpath, script_name):
         os.system(f"cd {dirpath} && {self.submission_command} {script_name}")
         
-    def reconverge_all(self):
+    def reconverge_all(self,
+                       HPC = "Setonix",
+                       VASP_version = "5.4.4",
+                       CPU = 128,
+                       walltime = 24,
+                       cpu_per_node=128):
         non_converged = self.reconverge_from_log_file()
         running_jobs_df = get_slurm_jobs_working_directories(self.user)
         running_queued_job_directories = running_jobs_df["Working Directory"].to_list()
@@ -69,7 +74,78 @@ class CalculationConverger():
                 
         return dirs_to_search_next_time
     
-    def reconverge(self, dirpath):
+    def reconverge(self, dirpath, type="SDRS"):
+
+        # Check if there are any error* and *tar* files in the directory
+        error_tar_files_exist = any("error" in f and "tar" in f for f in os.listdir(dirpath))
+
+        # Create error_run_n folder if error* and *tar* files exist
+        if error_tar_files_exist:
+            # Create error_run_n folder
+            latest_error_run_index = self.find_latest_error_run_index(dirpath)
+            error_run_folder_name = f"error_run_{latest_error_run_index + 1}"
+            error_run_folder_path = os.path.join(dirpath, error_run_folder_name)
+            os.makedirs(error_run_folder_path)
+            # Move files containing "error" and "tar" into the error_run_n folder
+            for f in os.listdir(dirpath):
+                if "error" in f and "tar" in f:
+                    shutil.move(os.path.join(dirpath, f), os.path.join(error_run_folder_path, f))
+                if f.endswith(".sh"):
+                    shutil.move(os.path.join(dirpath, f), os.path.join(error_run_folder_path, f))
+                    
+            orig_files_to_preserve = ["INCAR.orig", "POSCAR.orig", "KPOINTS.orig", "custodian.json"]
+            for og_file in orig_files_to_preserve:
+                if os.path.exists(os.path.join(dirpath, og_file)):
+                    shutil.move(os.path.join(dirpath, og_file), os.path.join(error_run_folder_path, og_file))
+               
+        if type=="SDRS":
+            self.reconverge_SDRS(dirpath, latest_error_run_index)
+        elif type=="DRS":
+            self.reconverge_DRS(dirpath, latest_error_run_index)
+            
+    # Function to find the latest error_run folder index
+    def find_latest_error_run_index(self, dirpath):
+        error_run_indices = [0]
+        for f in os.listdir(dirpath):
+            if f.startswith("error_run_") and f[11:].isdigit():
+                error_run_indices.append(int(f[11:]))
+        return max(error_run_indices)    
+    
+    def reconverge_DRS(self,
+                       dirpath,
+                       error_run_folder_path,
+                       HPC = "Setonix",
+                       VASP_version = "5.4.4",
+                       CPU = 128,
+                       walltime = 24,
+                       cpu_per_node=128
+                       ):
+        
+        relax1_files_exist = any(f.endswith(".relax_1") for f in os.listdir(dirpath))
+        relax2_files_exist = any(f.endswith(".relax_2") for f in os.listdir(dirpath))
+
+        if relax2_files_exist:
+            script_name = os.path.join(self.script_template_dir, "DRS_Custodian_2.sh")
+        elif relax1_files_exist:
+            script_name = os.path.join(self.script_template_dir, "DRS_Custodian_3.sh")
+        else:
+            script_name = os.path.join(self.script_template_dir, "DRS_Custodian.sh")
+
+        target_script_name = f"{os.path.basename(dirpath)}.sh"
+        job = jobfile(file_path = script_name,
+                    HPC = HPC,
+                    VASP_version = VASP_version,
+                    CPU = CPU,
+                    walltime = walltime,
+                    cpu_per_node=cpu_per_node)
+        
+        job.to_file(job_name=target_script_name,
+                    output_path=dirpath)
+        
+        # Submit to the queue using the error_run_n folder
+        self.submit_to_queue(error_run_folder_path, target_script_name)
+        
+    def reconverge_SDRS(self, dirpath):
         static1_files_exist = any(f.endswith(".static_1") for f in os.listdir(dirpath))
         relax1_files_exist = any(f.endswith(".relax_1") for f in os.listdir(dirpath))
         relax2_files_exist = any(f.endswith(".relax_2") for f in os.listdir(dirpath))
@@ -89,27 +165,7 @@ class CalculationConverger():
         shutil.copy(script_name, os.path.join(dirpath, target_script_name))
     
         self.submit_to_queue(dirpath, target_script_name)
-    
-    # def _get_latest_file_iteration(self, resubmit_log_filename = "resubmit.log_"):
-    #     # Check for existing resubmit.log_m files and find the largest m
-    #     resubmit_log_files = []
-    #     for filename in os.listdir(self.parent_dir):
-    #         if resubmit_log_filename in filename:
-    #             resubmit_log_files.append(filename)
-    #     max_integer = -1
-    #     if not resubmit_log_files:
-    #         return -1
-    #     else:
-    #         for log_file in resubmit_log_files:
-    #             if log_file.startswith(resubmit_log_filename):
-    #                 try:
-    #                     num_str = log_file[len(resubmit_log_filename):]
-    #                     num = int(num_str)
-    #                     max_integer = max(max_integer, num)
-    #                 except ValueError:
-    #                     pass  # Ignore non-integer parts after "resubmit.log_"
-    #         return max_integer
-                
+                 
     def reconverge_from_log_file(self):
         resubmit_log_file = os.path.join(self.parent_dir, "resubmit.log")        
         if os.path.isfile(resubmit_log_file):
