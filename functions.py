b14811806e12b6f555dff54f6c96ebffd78d6ae7
Han Mai <h.mai@mpie.de>
2023-12-12 00:57:24 +0100

    begin removing jobfile from functions.py

--- functions.py
+++ functions.py
@@ -5,6 +5,7 @@ import io
 from pymatgen.core import Structure
 from pymatgen.io.vasp.inputs import Potcar, Incar, Kpoints
 
+from utils.jobfile import jobfile
 potcar_library_path = "/root/POTCAR_Library/GGA"
 potcar_library_path = "/u/hmai/pyiron-resources-cmmc/vasp/potentials/potpaw_PBE"
 
@@ -52,89 +53,6 @@ def createFolder(directory, delete_folder='no'):
 
 def get_immediate_subdirectories(a_dir):
     return [f.path for f in os.scandir(a_dir) if f.is_dir() and os.path.basename(f) != ".ipynb_checkpoints"]
-
-# Create a job object for passing into createJobFolder
-class jobfile:
-    '''
-    Class for jobfile object for passing into createJobFolder
-
-    Attributes:
-    file_path: string, is the path to jobfile template (switch for normal VASP calcs vs DDEC6)
-    HPC: string, one of "Gadi", "Setonix", or "Magnus"  This ensures that correct jobsubmission script is used.
-    VASP_version: string (one of: "5.4.4", "5.4.4-metagga", "6.1.0", "6.2.1", "6.3.0", "6.3.1")
-                  NOT ALL ARE AVAILABLE ON ALL SYSTEMS
-                  for loading module - defaults to "5.4.4"
-                  AVAILABILITY:
-                  for Gadi (5.4.4, 5.4.4-metagga, 6.1.0, 6.2.1, 6.3.3)
-                  for Magnus (5.4.4, 6.2.0)
-                  for Setonix (5.4.4, 6.2.1, 6.3.0)
-    CPU: integer
-         number of CPUs to use in job,
-         Gadi: 48 is 1 node  (Only use in full nodes, as you are charged for full nodes)
-         Magnus: 24 is 1 node (Only use in full nodes, as you are charged for full nodes)
-         Setonix: 128 is 1 node (Charged on a per-cpu hour basis, not per-node like Gadi)
-    RAM: RAM to be allocated - this is only specified in the case of Gadi, Setonix + magnus do not need specification.
-    walltime: INTEGER ONLY
-              The walltime of the job in hours
-    '''
-    def __init__(self,
-                 file_path,
-                 HPC = "Gadi",
-                 VASP_version = "5.4.4",
-                 CPU = 192,
-                 cpu_per_node = 48,
-                 RAM = 64,
-                 walltime = 999,
-                 max_resubmissions = 999):
-        self.file_path = file_path
-        self.HPC = HPC
-        self.VASP_version = VASP_version
-        self.CPU = CPU
-        self.RAM = RAM
-        self.walltime = walltime
-        self.max_resubmissions = max_resubmissions
-        self.cpu_per_node = cpu_per_node
-
-    def to_file(self,\
-                job_name = 'template_job',\
-                output_path = os.path.join(os.getcwd(), "test")):
-        """
-        """
-
-        createFolder(output_path)
-
-        with open("%s" % (self.file_path), 'r') as fin :
-            filedata = fin.read()
-        fin = open("%s" % (self.file_path), "rt", newline="\n")
-        # Replace the target string
-        filedata = filedata.replace("{WALLTIMESTRING}", "%s:00:00" % self.walltime)
-        filedata = filedata.replace("{CPUSTRING}", str(self.CPU))
-        filedata = filedata.replace("{MAXCONVITERATIONS}", str(self.max_resubmissions-1))
-
-        # Only on GADI
-        filedata = filedata.replace("{MEMORYSTRING}", "%sGB" % self.RAM)
-
-        if self.CPU <= self.cpu_per_node:
-            filedata = filedata.replace("{NODESTRING}", "1")
-        else:
-            filedata = filedata.replace("{NODESTRING}", "%s" % int(self.CPU/self.cpu_per_node))
-            
-        filedata = filedata.replace("{CASESTRING}", "%s" % job_name)
-
-        if self.VASP_version == "5.4.4":
-            filedata = filedata.replace("{VASPMODULELOADSTRING}", 'module load vasp/%s' %  self.VASP_version)
-        else:
-            if self.HPC == "Setonix" and self.VASP_version in ["6.3.0", "6.2.1"]:
-                filedata = filedata.replace("{VASPMODULELOADSTRING}", 'module load vasp6/%s' % self.VASP_version)
-            else:
-                filedata = filedata.replace("{VASPMODULELOADSTRING}", 'module load vasp/%s' % self.VASP_version)
-
-        # Write the file out again
-        with open(os.path.join(output_path, job_name), 'w') as fout:
-            fout.write(filedata)
-
-        fin.close()
-        fout.close()
 
 def generateINCAR(structure,
                   path = os.path.join(os.getcwd(), "INCAR"),
